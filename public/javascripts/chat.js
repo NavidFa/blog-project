ea75176c51093d53397c1375d94412c06ad90cf5
NavidFa <navid.fallahazad@gmail.com>
2016-09-20 12:30:07 -0600

    chat and username

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -5,13 +5,11 @@ var socket = io()
 
 $("#target").submit(function(event){
  event.preventDefault();
- var message = $('#m').val();
+ var message = username +" "+"said"+": "+$('#m').val();
  socket.emit('chat message', message);
  $('#m').val('');
 })
-
 socket.on('sent message', function(message){
- $("#messages").append('<li>' + message + '</li>');
- $("#name").show();
+ $("#messages").append('<li>'+ message + '</li>');
 })
 })
